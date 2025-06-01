66bbce3788b6d362342fc71175ff810fe4f1b23c
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-06-01 21:37:04 +0100

    fix: Index.js

--- DAY 1/index.js
+++ DAY 1/index.js
@@ -1,4 +1,14 @@
 import express from "express";
 const app = express();
 const port = 3000;
+// const path = require('path')
 
+app.get("/", (req,res) => {
+    res.send("<h1>Welcome to 30days with Techyjaunt<h1>")
+    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
